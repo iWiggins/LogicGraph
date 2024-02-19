1d9287f8b4e4eaaa4b864275916e3460a43d090e
Ian Wiggins <ian.a.wiggins919@gmail.com>
2024-02-19 11:45:47 -0600

    added missing invalidation

--- LogicGraphTests/SpecialNOTTests.cs
+++ LogicGraphTests/SpecialNOTTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace LogicGraphTests
 {
     [TestClass]
-    public class ConnectedNOTTest
+    public class SpecialNOTTests
     {
         [TestMethod]
         public void NotGraphProvidesCorrectOutput()
@@ -23,15 +23,19 @@ namespace LogicGraphTests
             graph.ConnectGateToOutput(0, key);
 
             graph.FeedInputs(0, 0);
+            Assert.AreEqual(1, graph.GetValue(key));
             Assert.AreEqual(1, graph.GetOutputValue(0));
 
             graph.FeedInputs(0, 1);
+            Assert.AreEqual(1, graph.GetValue(key));
             Assert.AreEqual(1, graph.GetOutputValue(0));
 
             graph.FeedInputs(1, 0);
+            Assert.AreEqual(0, graph.GetValue(key));
             Assert.AreEqual(0, graph.GetOutputValue(0));
 
             graph.FeedInputs(1, 1);
+            Assert.AreEqual(0, graph.GetValue(key));
             Assert.AreEqual(0, graph.GetOutputValue(0));
         }
     }
